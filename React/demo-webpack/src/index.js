f6f350229520448a0da49841882de3073d86702a
Trần Khánh Gia Uy <uytran36@gmail.com>
2021-06-21 17:14:23 +0700

    update webpack

--- React/demo-webpack/src/index.js
+++ React/demo-webpack/src/index.js
@@ -1,5 +1,5 @@
-const sum = require('./math');
-
+import sum from "./math";
+import "./style.css";
 
 const total = sum(3, 5);
 document.write(total);
--- React/demo-webpack/webpack.config.js
+++ React/demo-webpack/webpack.config.js
@@ -1,12 +1,28 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const config = {
-    mode: 'none',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js', 
-        path: path.resolve(__dirname, 'build')
-    }
-}
-
-module.exports = config;
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.jsx?$/,
+      },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
